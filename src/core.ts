75151e038f47de46af5dff56c76b093c25b5a54e
juno <kraksin12@gmail.com>
2025-07-14 19:11:11 +0900

    feat: 배치 업데이트 적용
    마지막 업데이트만 task에서 한번에 구독 알림되고 업데이트 되도록 처리

--- src/core.ts
+++ src/core.ts
@@ -2,18 +2,18 @@ import type { Atom } from "./types";
 
 /**
  * Atom에서 클로저로 관리되고 있는 값을 반환하는 함수
- * 
+ *
  * @example
  * // atom에서 관리되고 있는 값을 가져오기
  * const count = get(countAtom);
  * console.log(count);
  *
- * // atom에서 관리되고 있는 값을 가져와 업데이트 
+ * // atom에서 관리되고 있는 값을 가져와 업데이트
  * set(countAtom, get(countAtom) + 1);
  */
 export const get = <T>(atom: Atom<T>) => {
   return atom._getValue();
-}
+};
 
 /**
  * Atom에서 클로저로 관리되고 있는 값을 업데이트 하는 함수
@@ -23,11 +23,11 @@ export const get = <T>(atom: Atom<T>) => {
  */
 export const set = <T>(atom: Atom<T>, value: T) => {
   atom._setValue(value);
-}
+};
 
 /**
  * Atom에서 클로저로 관리되고 있는 값의 변경의 감지를 구독하는 함수
- * 
+ *
  * @example
  * const unsubscribe = subscribe(countAtom, (value => {
  *  console.log(`value changed to ${value}`);
@@ -39,11 +39,24 @@ export const set = <T>(atom: Atom<T>, value: T) => {
  */
 export const subscribe = <T>(atom: Atom<T>, callback: (value: T) => void) => {
   return atom._subscribe(callback);
-}
+};
 
 export const createAtom = <T>(initialValue: T): Atom<T> => {
   let curValue = initialValue;
   let subscribers = new Set<(value: T) => void>();
+
+  let isBatching = false;
+
+  const batchUpdate = () => {
+    if (!isBatching) {
+      isBatching = true;
+
+      queueMicrotask(() => {
+        subscribers.forEach((callback) => callback(curValue));
+        isBatching = false;
+      });
+    }
+  };
 
   return {
     _getValue: () => curValue,
@@ -53,14 +66,14 @@ export const createAtom = <T>(initialValue: T): Atom<T> => {
       }
 
       curValue = value;
-      subscribers.forEach(callback => callback(value));
+      batchUpdate();
     },
     _subscribe: (callback: (value: T) => void) => {
       subscribers.add(callback);
-      
+
       return () => {
         subscribers.delete(callback);
-      }
-    }
-  }
-}
+      };
+    },
+  };
+};
